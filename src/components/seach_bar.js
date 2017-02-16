1242cdede56903fe8e1789497fb4b90262598952
Tlangelani Mboweni <mbowenit@gmail.com>
2017-02-17 00:55:02 +0200

    end lesson 1

--- src/components/seach_bar.js
+++ src/components/seach_bar.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-    return <input />
-};
+// Functional component
+// const SearchBar = () => {
+//     return <input placeholder="please enter your name..." />
+// };
+
+// Class base component
+class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { term: '' };
+    }
+    render() {
+        return (
+            <div>
+                <input onChange={this.onInputChange.bind(this)} placeholder="address" />
+                Value: {this.state.term}
+            </div>
+        );
+    }
+
+    onInputChange(event) {
+        this.setState({term: event.target.value});
+    }
+}
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/seach_bar';
 
 // google api key
 const API_KEY = 'AIzaSyAZo7WXm48b0oLeQJoxi503ITaX90EVFlY';
 
 // create new component using jsx, this should produce HTML.
 const App = () => {
-    return <div>Hello</div>;
+    return <div>
+        <SearchBar />
+    </div>;
 };
 
 // take this component's generated HTML and put it on the page.
